4213950e2eccfe56be7ae504be8cf21bcd80e1eb
Enrique Román García Espino <70731354+ZorvShadow@users.noreply.github.com>
2023-11-22 18:30:46 -0600

    Refactor code to calculate square root using Newton's method and calculate vector operations

--- src/Cuadrado.java
+++ src/Cuadrado.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Cuadrado {
+import java.util.Scanner;
+
+public class Cuadrado {
+    public static double raizNewton(double numero, double x) {
+        if (x < 0) return -1d;
+        double fx = x * x - numero;
+        double devFx = 2*x;
+        double x1 = x - (fx/devFx);
+        System.out.print("Intento: " + x1 + "\n");
+        double dif = Math.abs(x1 - x);
+        if (dif < 0.0000000000001) {
+            return x1;
+        } else {
+            return raizNewton(numero, x1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("¿Qué raíz cuadrada deseas calcular? ");
+        double num = scanner.nextDouble();
+
+        System.out.print(raizNewton(num, num/2));
+    }
 }
--- src/Vectores.java
+++ src/Vectores.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Vectores {
+import java.util.Scanner;
+
+public class Vectores {
+    public static float productoPunto(float[] vector1, float[] vector2) {
+        float resultado = 0;
+        for (int i = 0; i < 3; i++) {
+            resultado += vector1[i] * vector2[i];
+        }
+        return resultado;
+    }
+
+    public static float[] productoCruz(float[] vector1, float[] vector2) {
+        float[] resultado = new float[3];
+        resultado[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
+        resultado[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
+        resultado[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        do {
+            float[] vector1 = new float[3];
+            float[] vector2 = new float[3];
+
+            // Solicitar input del usuario para el vector1
+            System.out.println("Ingrese las componentes del primer vector:");
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Componente " + (i + 1) + ": ");
+                vector1[i] = scanner.nextFloat();
+            }
+
+            // Solicitar input del usuario para el vector2
+            System.out.println("Ingrese las componentes del segundo vector:");
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Componente " + (i + 1) + ": ");
+                vector2[i] = scanner.nextFloat();
+            }
+
+            // Calcular y mostrar el producto punto
+            float productoPuntoResultado = productoPunto(vector1, vector2);
+            System.out.println("Producto Punto: " + productoPuntoResultado);
+
+            // Calcular y mostrar el producto cruz
+            float[] productoCruzResultado = productoCruz(vector1, vector2);
+            System.out.println("Producto Cruz: " + productoCruzResultado[0] + "i, " + productoCruzResultado[1] + "j, " + productoCruzResultado[2] + "k");
+
+            System.out.print("Desea continuar? (S/N) ");
+        } while (scanner.next().charAt(0) == 's');
+        // Cerrar el scanner
+        scanner.close();
+    }
 }
